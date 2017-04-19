d67dd33d4b72272853ef13738c272a8b782b557f
jusalsingh <9994068@student.swin.edu.au>
2017-04-19 18:10:21 +1000

    Add files via upload

---
+++ Staff_Login.sql
@@ -0,0 +1,29 @@
+create table Staff_Login (
+	Staff_ID VARCHAR(50),
+	Password VARCHAR(50)
+);
+insert into Staff_Login (Staff_ID, Password) values ('wanderson0', '4OsvsnmTi');
+insert into Staff_Login (Staff_ID, Password) values ('kspencer1', 'w1RokXN');
+insert into Staff_Login (Staff_ID, Password) values ('cmyers2', '9xUzbQH');
+insert into Staff_Login (Staff_ID, Password) values ('bramirez3', 'dWJ1CvAGKE');
+insert into Staff_Login (Staff_ID, Password) values ('ldixon4', 'U4CxEck');
+insert into Staff_Login (Staff_ID, Password) values ('jray5', 'ZtPVRr');
+insert into Staff_Login (Staff_ID, Password) values ('ebishop6', 'UhcjMV');
+insert into Staff_Login (Staff_ID, Password) values ('ehenderson7', '2HAYAHf');
+insert into Staff_Login (Staff_ID, Password) values ('tspencer8', 'MIsMoQBgDuJC');
+insert into Staff_Login (Staff_ID, Password) values ('rcrawford9', 'QnUYUVELLaD');
+insert into Staff_Login (Staff_ID, Password) values ('bwatsona', 'mVw3iDuiUjZ');
+insert into Staff_Login (Staff_ID, Password) values ('cpowellb', 'e29p2TE');
+insert into Staff_Login (Staff_ID, Password) values ('hwatsonc', 'hjBpEZ');
+insert into Staff_Login (Staff_ID, Password) values ('rbennettd', 'F9eyxNViiH');
+insert into Staff_Login (Staff_ID, Password) values ('kgutierreze', '8Y2qM7CEEN');
+insert into Staff_Login (Staff_ID, Password) values ('hburkef', '7vJZL32M');
+insert into Staff_Login (Staff_ID, Password) values ('pcoleg', 'YNmrGHrnZkN');
+insert into Staff_Login (Staff_ID, Password) values ('mkimh', 'C8nHinR3Mw');
+insert into Staff_Login (Staff_ID, Password) values ('agarzai', 'ldAszp');
+insert into Staff_Login (Staff_ID, Password) values ('sbennettj', '94hmdP');
+insert into Staff_Login (Staff_ID, Password) values ('tsandersk', 'iL2amM81');
+insert into Staff_Login (Staff_ID, Password) values ('ccampbelll', 'zEyiEFErEtCY');
+insert into Staff_Login (Staff_ID, Password) values ('apaynem', 'LpMTnzAzf');
+insert into Staff_Login (Staff_ID, Password) values ('hfordn', 'MLM8HX5L');
+insert into Staff_Login (Staff_ID, Password) values ('alawsono', 'reEhZGI');
