758dbacba0f2cea18314781c6f160d2008266835
koalaah <100859189@student.swin.edu.au>
2017-04-27 00:52:19 +1000

    fields added to addsales page

---
+++ addsales.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html XMLns="http://www.w3.org/1999/xHTML">
+<head>
+	<title>Add Sales Record</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+	<link href="css/add-sales.css" rel="stylesheet" />
+</head>
+<body>
+
+<?php
+session_start();
+$default = "";
+$default = $_SESSION['$login_user'];
+
+
+
+  // define variables and set to empty values
+  $totalpriceErr = $saledateErr = "";
+  $staffno = $saledate = $totalprice = "";
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //statement to see if name field is empty. If it is, populate error variable. If not, put data into name variable
+
+      //$inum = isset($_POST['inum']) ? $_POST['inum'] : "";
+
+  //statement to see if password field is empty. If it is, populate error variable. If not, put data into password variable
+
+     $staffno = isset($_POST['staffno']) ? $_POST['staffno'] : "";
+
+  //statement to see if password confirmation field is empty. If it is, populate error variable. If not, put data into confirmation password variable
+ if (empty($_POST['saledate'])) {
+      $saledateErr = "Sale Date is required";
+    } else {
+      $saledate = isset($_POST['saledate']) ? $_POST['saledate'] : "";
+  }
+
+  //statement to see if email field is empty. If it is, populate error variable. If not, put data into email variable
+ if (empty($_POST['totalprice'])) {
+      $totalpriceErr = "Total Price is required";
+    } else {
+      $totalprice = isset($_POST['totalprice']) ? $_POST['totalprice'] : "";
+  }
+}
+?>
+
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="cover.html">PHP Inc.</a>
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li class="dropdown active">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">SALES <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="addsales.php">ADD SALES</a></li>
+						<li><a href="edit.php">EDIT SALES</a></li>
+						<li><a href="viewsales.php">DISPLAY SALES</a></li>
+					</ul>
+				</li>
+				<li><a href="#">GOODS RECEIVED</a></li>
+				<li><a href="#">REPORTING</a></li>
+				<li><a href="#">SALES</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">User <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Create New User</a></li>
+						<li><a href="logout.php">Log Out</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+	<form action="" method="post">
+		<div class="row">
+			<div class="col-xs-12">
+				<H1 class="text-center">Add Sales Record</H1>
+			</div>
+			<div class="col-xs-6">
+				<div class="form-row">
+						<label for="staffno">Item Code</label>
+						<input type="text" class="form-control" name="itemcode" id="itemcode" />
+				</div>
+				<div class="form-row">
+						<label for="staffno">Description</label>
+						<textarea class="form-control" rows="5" name="desc" id="desc"></textarea>
+				</div>
+				<div class="form-row">
+					<label for="totalprice">RRP</label>
+					<div class="input-group">
+						<input type="number" min="1" step="any" class="form-control" name="rrp" id="rrp" /><span class="error"><?php echo $totalpriceErr;?></span>
+					</div>
+				</div>
+				<div class="form-row">
+					<label for="totalprice">Total Price</label>
+					<div class="input-group">
+						<input type="number" min="1" step="any" class="form-control" name="totalprice" id="totalprice" /><span class="error"><?php echo $totalpriceErr;?></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6">
+				<div class="form-row">
+						<label for="staffno">Staff Number</label>
+						<input type="text" class="form-control" name="staffno" id="staffno" value="<?php echo $default; ?>"/>
+				</div>
+				<div class="form-row">
+					<label for="saledate">Date of Sale</label>
+					<input type="date" class="form-control" placeholder="YYYY-MM-DD" name="saledate" id="saledate" /><span class="error"><?php echo $saledateErr;?></span>
+				</div>
+			</div>
+			<div class="col-xs-12">
+				<div class="form-row">
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+			</div>
+		</div>
+	</form>
+</div><!-- /.form-container -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/angular.min.js"></script>
+</body>
+<?php
+ //if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      //$servername = "localhost";
+	  //$username = "dp2";
+	  //$password = "phpdp2";
+	  //$dbname = "dp2php";
+
+	  // Create connection
+	  //$conn = new mysqli($servername, $username, $password, $dbname);
+	  // Check connection
+	  //if ($conn === false) {
+	    //die("Connection failed: " . $conn->connect_error);}
+
+  //SQl query to check if user already exists
+  include 'connection.php';
+
+  connection();
+
+  if(!empty($staffno) && !empty($saledate) && !empty($totalprice)) {
+
+      //SQL statement to insert new record of user
+      $sql = "INSERT INTO Sales (SalesDate, StaffNo, TotalPrice)
+      VALUES ('$saledate', $staffno, $totalprice)";
+
+
+      if($conn->query($sql) === TRUE){
+       echo "Sales Record successfully added.";
+      }
+      else {
+        echo "ERROR: Could not execute insert." . $conn->error;
+      }
+
+  $conn->close();
+    }
+  // }
+?>
+</html>
