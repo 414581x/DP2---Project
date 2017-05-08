5b76b21b6cf7c7e0e56cc08da612bf66e56e5072
414581x <414581x@student.swin.edu.au>
2017-05-09 07:34:11 +1000

    Working login page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html XMLns="http://www.w3.org/1999/xHTML">
 <head>
-	<title>Login</title>
+	<title>PHP Inc.</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link href="css/bootstrap.min.css" rel="stylesheet" />
@@ -17,11 +17,12 @@ header("location: addsales.php");
 // define variables and set to empty values
 $staffnoErr = $passwordErr = "";
 $LoginNumber = $LoginPassword = "";
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		if (empty($_POST['staffno'])) {
-			$staffnoErr = "Staff Number is required";
-		} else {
-			$LoginNumber=isset($_POST['staffno']) ? $_POST['staffno'] : "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+	if (empty($_POST['staffno'])) {
+		$staffnoErr = "Staff Number is required";
+	} else {
+		$LoginNumber=isset($_POST['staffno']) ? $_POST['staffno'] : "";
 	}
 	//statement to see if password field is empty. If it is, populate error variable. If not, put data into password variable
 	if (empty($_POST['lpassword'])) {
@@ -29,8 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	} else {
 		 $LoginPassword = isset($_POST['lpassword']) ? $_POST['lpassword'] : "";
 	}
-
-	}
+}
 ?>
 
 <div class="jumbotron text-center">
@@ -42,51 +42,69 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	<div class="row">
 		<div class="col-xs-12">
 			<form action="" method="post">
-				<div class="form-row"><H1>Login</H1></div>
+				<div class="form-row"><H1 class="text-center">Login</H1></div>
 				<div class="form-row">
 					<label for="staffno">Staff Number</label>
 					<input type="text" class="form-control" name="staffno" id="staffno" /><span class="error"><?php echo $staffnoErr;?></span>
 				</div>
 				<div class="form-row">
 					<label for="lpassword">Password</label>
-					<input type="text" class="form-control" name="lpassword" id="lpassword" /><span class="error"><?php echo $passwordErr;?></span>
+					<input type="password" class="form-control" name="lpassword" id="lpassword" /><span class="error"><?php echo $passwordErr;?></span>
 				</div>
 			<br/>
 				<div class="form-row">
 					<button type="submit" class="btn btn-primary" value="Login">Log In</button>
 				</div>
+				<div class="form-row">
+					<p class="text-center">Don't have an account?</p>
+					<a href="register.php" class="btn btn-primary">Create New User</a>
+				</div>
 			</form>
-		</div>
-
-		<div class="col-xs-12 center-block">
-			<p class="text-center">Don't have an account?</p>
-			<a href="#" class="btn btn-primary">Create New User</a>
 		</div>
 	</div>
 </div>
 
-<?php
+<?php 
 //Starting a session
 session_start();
 //if fields are not empty, execute
 if (!empty($LoginNumber) && !empty($LoginPassword))
 {
 	//include the php file with the connection details
-	include 'connection.php';
+	//include 'connection.php';
+
+	$servername = "localhost";
+	$username = "dp2";
+	$password = "phpdp2";
+	$dbname = "dp2php";
+
+	
+  // Create connection
+  $conn = mysql_connect($servername, $username, $password);
+  $db = mysql_select_db($dbname, $conn);
+
 	
 
 	//SQL query to select the correct table row where the customer number and password match
-	$sql = "SELECT * FROM Staff WHERE StaffID = $LoginNumber AND Password = '$LoginPassword'" ;
-//if($conn->query($sql) === TRUE){
-	$_SESSION['$login_user']=$LoginNumber; // Initializing Session
-	header("location: addsales.php"); // Redirecting to addsales.php
+	//$sql = "SELECT * FROM Staff WHERE StaffID = '$LoginNumber' AND lpassword = '$LoginPassword'" ;
+	
+	//if($conn->query($sql) === TRUE){
+	//$_SESSION['$login_user']=$LoginNumber; // Initializing Session
+	//header("location: addsales.php"); // Redirecting to addsales.php
 
-	//}
-	//else {
+	$query = mysql_query("SELECT * FROM Staff where StaffID = '$LoginNumber' and Password = '$LoginPassword'", $conn) ;
+	  $rows = mysql_num_rows($query);
+	  if ($rows == 1) {
+	  $_SESSION['$login_user']=$LoginNumber; // Initializing Session
+	  header("location: addsales.php"); // Redirecting to request.php
+  
+  }
+	
+	else {
 		//If no user is found, show error message
-		//echo "Staff Number or Password is invalid";
-	//}
-	$conn->close(); // Closing Connection
+		echo "Staff Number or Password is invalid";
+	}
+	 mysql_close($conn); // Closing Connection
 }
 ?>
 </body>
