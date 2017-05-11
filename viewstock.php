4d8004eef394d5bdf47e84c303b030bc00056ae2
koalaah <100859189@student.swin.edu.au>
2017-05-11 10:06:06 +1000

    Look fix

--- viewstock.php
+++ viewstock.php
@@ -10,124 +10,133 @@
 </head>
 <body>
 
-<?php 
+<?php
   $servername = "localhost";
   $username = "dp2";
   $password = "phpdp2";
   $dbname = "dp2php";
   $datatable = "Items"; // MySQL table name
   $results_per_page = 20; // number of results per page
- 
+
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
-  // Check connection
- 	if ($conn === false) {
-    die("Connection failed: " . $conn->connect_error);}
-if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
-$start_from = ($page-1) * $results_per_page;
-$sql = "SELECT ItemID, CategoryDescription, ItemName, Qty, Price FROM ".$datatable.", Category WHERE Items.CategoryID = Category.CategoryID ORDER BY ItemName ASC LIMIT $start_from, ".$results_per_page;
-		
-		$rs_result = $conn->query($sql);
-?> 
-    
-    
 
+	// Check connection
+ 	if ($conn === false) { die("Connection failed: " . $conn->connect_error); }
+	if (isset($_GET["page"])) { $page = $_GET["page"]; }
+	else { $page = 1; };
 
+	$start_from = ($page-1) * $results_per_page;
 
-<nav class="navbar navbar-default" role="navigation">
-<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="cover.html">PHP Inc.</a>
-		</div>
-	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		<ul class="nav navbar-nav">
-			<li class="dropdown active">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">SALES <span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="addsales.php">ADD SALES</a></li>
-					<li><a href="edit.php">EDIT SALES</a></li>
-					<li><a href="viewsales.php">DISPLAY SALES</a></li>
-				</ul>
-			</li>
-			<li><a href="#">GOODS RECEIVED</a></li>
-			<li><a href="#">REPORTING</a></li>
-			<li><a href="#">SALES</a></li>
-		</ul>
-		<ul class="nav navbar-nav navbar-right">
-			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">User <span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="#">Create New User</a></li>
-					<li><a href="logout.php">Log Out</a></li>
-				</ul>
-			</li>
-		</ul>
-	</div><!-- /.navbar-collapse -->
-</div><!-- /.container-fluid -->
-</nav>
+	$sql = "SELECT ItemID, CategoryDescription, ItemName, Qty, Price
+					FROM ".$datatable.", Category
+					WHERE Items.CategoryID = Category.CategoryID
+					ORDER BY ItemID ASC LIMIT $start_from, ".$results_per_page;
 
-<div class="container">
+	$rs_result = $conn->query($sql);
+
+	session_start();
+	$default = "";
+	$default = $_SESSION['$login_user'];
+?>
+
+	<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="cover.html">PHP Inc.</a>
+			</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">SALES <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="addsales.php">ADD SALES</a></li>
+						<li><a href="edit.php">EDIT SALES</a></li>
+						<li><a href="viewsales.php">VIEW SALES</a></li>
+					</ul>
+				</li>
+				<li class="dropdown active">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">STOCK <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="addstock.php">ADD STOCK ITEM</a></li>
+						<li><a href="viewstock.php">VIEW STOCK ITEMS</a></li>
+						<li><a href="editstock.php">EDIT STOCK ITEMS</a></li>
+					</ul>
+				</li>
+				<li><a href="#">REPORTING</a></li>
+				<li><a href="#">PREDICTION</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span><?php echo $default;?></span> <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Create New User</a></li>
+						<li><a href="logout.php">Log Out</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+	</nav>
+
+	<div class="container">
 		<form action="" method="post">
-			<div class="form-row"><H1>View Stock Items</H1></div>
-	<div class="form-row">
-				
-<div class="form-row">
-<table border="1" cellpadding="4">
-<tr>
-    <td bgcolor="#CCCCCC"><strong>ItemID</strong></td>
-    <td bgcolor="#CCCCCC"><strong>Category</strong></td><td bgcolor="#CCCCCC"><strong>Name</strong></td><td bgcolor="#CCCCCC"><strong>Qty</strong></td><td bgcolor="#CCCCCC"><strong>Price</strong></td><td bgcolor="#CCCCCC"><strong>Options</strong></td></tr>
-<?php 
- while($row = $rs_result->fetch_assoc()) {
-?> 
-            <tr>
-            <td><? echo $row["ItemID"]; ?></td>
-            <td><? echo $row["CategoryDescription"]; ?></td>
-            <td><? echo $row["ItemName"]; ?></td>
-            <td><? echo $row["Qty"]; ?></td>
-            <td><? echo $row["Price"]; ?></td>
-            <td><? echo "<a href= 'edit.php?ItemID=$row[ItemID]'>Edit </a><a href= 'delete.php?ItemID=$row[ItemID]'>Delete</a>"; ?></td>
-            </tr>
-<?php 
-}; 
-?> 
-</table>
-</div>
-
-
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 class="text-center">View Stock Details</h1><hr>
 			</div>
-			<br/>
-			<div class="form-row">
-				<button type="submit" class="btn btn-primary">Submit</button>
-			</div>
-		</form>
-		<form method="get" action="csv.php">
-		<p>Export as CSV:<input id="submit" type="submit" value="Export"/></p>
-	</form>
-</div><!-- /.form-container -->
+		</div>
+		<div class="col-xs-12">
+			<div class="table-responsive">
+				<table class="table table-hover">
+					<thead class="thead-default">
+						<tr>
+		  				<th>ItemID</th>
+		  				<th>Category</th>
+							<th>Name</th>
+							<th>Qty</th>
+							<th>Price</th>
+							<th>Options</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php while($row = $rs_result->fetch_assoc()) { ?>
+					  <tr>
+					    <td><? echo $row["ItemID"]; ?></td>
+					    <td><? echo $row["CategoryDescription"]; ?></td>
+					    <td><? echo $row["ItemName"]; ?></td>
+					    <td><? echo $row["Qty"]; ?></td>
+					    <td><? echo $row["Price"]; ?></td>
+					    <td><? echo "<a href= 'edit.php?ItemID=$row[ItemID]'>Edit </a><a href= 'delete.php?ItemID=$row[ItemID]'>Delete</a>"; ?></td>
+					  </tr>
+					<?php }; ?>
+					</tbody>
+				</table>
+			</div><!-- /.table -->
+		</div>
+	</div><!-- /.container -->
+
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/angular.min.js"></script>
 
-	<?php 
-$sql = "SELECT COUNT(ItemID) AS total FROM ".$datatable;
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
-$total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
-  
-for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
+	<?php
+		$sql = "SELECT COUNT(ItemID) AS total FROM ".$datatable;
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		$total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
+
+		for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
             echo "<a href='viewstock.php?page=".$i."'";
             if ($i==$page)  echo " class='curPage'";
-            echo ">".$i."</a> "; 
-}; 
-?>
+            echo ">".$i."</a> ";
+		};
+	?>
 </body>
-<?php 
- 
-?>
 </html>
