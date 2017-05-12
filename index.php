7b8aa750626e6ad821833166beed9f2dd65384c7
koalaah <100859189@student.swin.edu.au>
2017-05-12 14:00:45 +1000

    Login page Final

--- index.php
+++ index.php
@@ -38,30 +38,41 @@
 	<p>Sales Recording and Prediction System</p>
 </div>
 
-<div class="container">
+<div class="container block">
 	<div class="row">
 		<div class="col-xs-12">
-			<form action="" method="post">
-				<div class="form-row"><H1 class="text-center">Login</H1></div>
-				<div class="form-row">
-					<label for="staffno">Staff Number</label>
-					<input type="text" class="form-control" name="staffno" id="staffno" /><span class="error"><?php echo $staffnoErr;?></span>
+			<H1 class="text-center">Login</H1>
+			<hr>
+		</div>
+	</div>
+		<div class="row col-xs-3 center">
+			<form action="" method="POST">
+				<div class="form-group">
+					<div class="col-xs-12">
+						<label class="control-label" for="staffno">Staff Number</label>
+						<input type="text" class="form-control" name="staffno" id="staffno" /><span class="error"><?php echo $staffnoErr;?></span>
+					</div>
 				</div>
-				<div class="form-row">
-					<label for="lpassword">Password</label>
-					<input type="password" class="form-control" name="lpassword" id="lpassword" /><span class="error"><?php echo $passwordErr;?></span>
+				<div class="form-group">
+					<div class="col-xs-12">
+						<label class="control-label" for="lpassword">Password</label>
+						<input type="password" class="form-control" name="lpassword" id="lpassword" /><span class="error"><?php echo $passwordErr;?></span>
+					</div>
 				</div>
-			<br/>
-				<div class="form-row">
-					<button type="submit" class="btn btn-primary" value="Login">Log In</button>
+				<br/>
+				<div class="form-group">
+					<div class="col-xs-12" id="spacing">
+						<button type="submit" class="btn btn-primary btn-block" value="Login">Log In</button>
+					</div>
 				</div>
-				<div class="form-row">
-					<p class="text-center">Don't have an account?</p>
-					<a href="register.php" class="btn btn-primary">Create New User</a>
+				<div class="form-group">
+					<div class="col-xs-12" id="spacing">
+						<p class="text-center">Don't have an account?</p>
+						<a href="register.php" class="btn btn-primary btn-block">Create New User</a>
+					</div>
 				</div>
 			</form>
 		</div>
-	</div>
 </div>
 
 <?php
@@ -72,37 +83,30 @@ if (!empty($LoginNumber) && !empty($LoginPassword))
 {
 	//include the php file with the connection details
 	//include 'connection.php';
-
 	$servername = "localhost";
 	$username = "dp2";
 	$password = "phpdp2";
 	$dbname = "dp2php";
-
-
   // Create connection
   $conn = mysql_connect($servername, $username, $password);
   $db = mysql_select_db($dbname, $conn);
-
-
-
 	//SQL query to select the correct table row where the customer number and password match
 	//$sql = "SELECT * FROM Staff WHERE StaffID = '$LoginNumber' AND lpassword = '$LoginPassword'" ;
-
 	//if($conn->query($sql) === TRUE){
 	//$_SESSION['$login_user']=$LoginNumber; // Initializing Session
 	//header("location: addsales.php"); // Redirecting to addsales.php
-
 	$query = mysql_query("SELECT * FROM Staff where StaffID = '$LoginNumber' and Password = '$LoginPassword'", $conn) ;
 	  $rows = mysql_num_rows($query);
 	  if ($rows == 1) {
 	  $_SESSION['$login_user']=$LoginNumber; // Initializing Session
 	  header("location: addsales.php"); // Redirecting to request.php
-
   }
-
 	else {
 		//If no user is found, show error message
-		echo "Staff Number or Password is invalid";
+		echo "<div class='container'><div class'row'>";
+		echo "<div class='col-xs-3 alert text-center'>Staff Number or Password is Invalid</div>";
+		echo "</div></div>";
+		// echo "Staff Number or Password is invalid";
 	}
 	 mysql_close($conn); // Closing Connection
 }
